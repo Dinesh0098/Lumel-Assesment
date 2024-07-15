484c491769f54e0a336df8a0172667a9c5216c44
Dinesh <dinesh0098@gmail.com>
2024-07-15 23:06:08 +0530

    Basic row value calculation and input validation.

--- src/components/Table/TableRow/index.js
+++ src/components/Table/TableRow/index.js
@@ -1,24 +1,79 @@
-import { Button, TableCell, TableRow } from "@mui/material";
+import { Button, TableCell, TableRow, TextField } from "@mui/material";
+import { useState } from "react";
+
+const ADD_VALUE_ACTION = "addValue";
 
 export default function CustomTableRow({ row }) {
-  const rowChildren = row.children;
+  const [currentRowData, setCurrentRowData] = useState({ ...row });
+  const [inputValue, setInputValue] = useState();
+
+  const { label, value, children, gain } = currentRowData;
+  const rowChildren = children;
+
+  const onInputValueChange = (event) => {
+    const value = event.target.value;
+
+    // Remove non-numeric characters, spaces, and ensure no leading zeros
+    const sanitizedValue = value.replace(/[^0-9]/g, "").replace(/^0+/, "");
+
+    // Set the sanitized value.
+    setInputValue(sanitizedValue ?? 0);
+  };
+
+  const calculateValueChange = (action) => {
+    let addingValue = 0;
+
+    if (action === ADD_VALUE_ACTION) {
+      addingValue = Number(value ?? 0) + Number(inputValue);
+    } else {
+      const percentage = (value * inputValue) / 100;
+      addingValue = Number(value ?? 0) + Number(percentage);
+    }
+
+    const gain = ((addingValue - row.value) / row.value) * 100;
+
+    // Spreading data to remove the object reference
+    setCurrentRowData({
+      ...{
+        ...currentRowData,
+        value: addingValue,
+        gain,
+      },
+    });
+  };
+
   return (
     <>
       <TableRow>
-        <TableCell>{row?.label}</TableCell>
-        <TableCell>{row?.value}</TableCell>
-        <TableCell>Input</TableCell>
+        <TableCell>{label}</TableCell>
+        <TableCell>{value?.toFixed(2)}</TableCell>
         <TableCell>
-          <Button variant="outlined" size="small">
+          <TextField
+            size="small"
+            placeholder="Add value"
+            onChange={onInputValueChange}
+            value={inputValue}
+          />
+        </TableCell>
+        <TableCell>
+          <Button
+            variant="outlined"
+            size="small"
+            onClick={() => calculateValueChange()}
+          >
             Allocate %
           </Button>
         </TableCell>
         <TableCell>
-          <Button variant="outlined" size="small">
+          <Button
+            variant="outlined"
+            size="small"
+            onClick={() => calculateValueChange(ADD_VALUE_ACTION)}
+          >
             Allocate Val
           </Button>
         </TableCell>
-        <TableCell>some</TableCell>
+        <TableCell>{gain?.toFixed(2) ?? "-"}</TableCell>
       </TableRow>
       {rowChildren?.length
         ? rowChildren.map((childRow) => (
